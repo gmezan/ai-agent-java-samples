ac10a3ee658b088ec13e416b76298c27a6274576
gmezan <meza.gustavo@pucp.edu.pe>
2025-03-25 18:44:00 -0500

    starting rag sample

---
+++ java-azure-openai-rag-agent/src/main/java/com/gmezan/ai/skagent/controller/ChatController.java
@@ -0,0 +1,101 @@
+package com.gmezan.ai.skagent.controller;
+
+import com.gmezan.ai.skagent.model.web.ChatRequest;
+import com.gmezan.ai.skagent.model.web.ResponseModel;
+import com.gmezan.ai.skagent.service.ChatService;
+import com.microsoft.semantickernel.services.KernelContent;
+import com.microsoft.semantickernel.services.chatcompletion.AuthorRole;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@CrossOrigin
+public class ChatController {
+	private final ChatService chatService;
+
+	@PostMapping(value = "/api/chat", produces = MediaType.APPLICATION_JSON_VALUE,
+		consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Mono<ResponseModel> generate(@RequestBody ChatRequest request) {
+		return Mono.justOrEmpty(request)
+				.map(this::toChatHistory)
+				.flatMap(chatService::getCompletion)
+				.flatMapMany(Flux::fromIterable)
+				.last()
+				.mapNotNull(ChatMessageContent::getContent)
+				.map(content -> this.mapResponse(0, content, buildContext()));
+	}
+
+	@PostMapping(value = "/api/chat", produces = MediaType.APPLICATION_NDJSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Flux<ResponseModel> generateStream(@RequestBody ChatRequest request) {
+
+		log.info("Request: {}", request);
+
+		return Mono.justOrEmpty(request)
+				.map(this::toChatHistory)
+				.flatMapMany(chatService::getCompletionStream)
+				.mapNotNull(KernelContent::getContent)
+				.onErrorComplete()
+				.index()
+				.map(tuple -> mapResponse(tuple.getT1() + 1, tuple.getT2(), null))
+				.startWith(mapResponse(0,"", buildContext()));
+	}
+
+	private ChatHistory toChatHistory(ChatRequest request) {
+		ChatHistory chatHistory = new ChatHistory();
+		chatHistory.addAll(
+				request.getMessages()
+						.stream()
+						.map(m -> new ChatMessageContent<>(
+								AuthorRole.valueOf(m.getRole().toUpperCase()),
+								m.getContent()
+						))
+						.collect(Collectors.toUnmodifiableList())
+		);
+
+		return chatHistory;
+	}
+
+	private Object buildContext() {
+		final Map<String, Object> context = new HashMap<>();
+
+		// system prompt/ chat history?
+		context.put("thoughts", "");
+		// from search
+		context.put("data_points", List.of());
+
+		return context;
+	}
+
+	// citations go in `content` in the following format [<file-name>#page=<page-number>]
+	private ResponseModel mapResponse(long order, String content, Object context) {
+
+		return ResponseModel.builder()
+				.choices(List.of(ResponseModel.Choice.builder()
+						.context(context)
+						.index((int) order)
+						.delta(ResponseModel.Message.builder()
+								.content(content)
+								.role(AuthorRole.ASSISTANT.name())
+								.build())
+						.message(ResponseModel.Message.builder()
+								.content(content)
+								.role(AuthorRole.ASSISTANT.name())
+								.build())
+						.build()))
+				.build();
+	}
+}
---
+++ java-azure-openai-rag-agent/src/main/java/com/gmezan/ai/skagent/service/ChatService.java
@@ -0,0 +1,71 @@
+package com.gmezan.ai.skagent.service;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.google.gson.Gson;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.contextvariables.ContextVariableTypeConverter;
+import com.microsoft.semantickernel.contextvariables.ContextVariableTypes;
+import com.microsoft.semantickernel.orchestration.InvocationContext;
+import com.microsoft.semantickernel.orchestration.InvocationReturnMode;
+import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
+import com.microsoft.semantickernel.orchestration.ToolCallBehavior;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
+import com.microsoft.semantickernel.services.chatcompletion.StreamingChatContent;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+public class ChatService {
+	private final OpenAIAsyncClient openAIAsyncClient;
+	private final ChatCompletionService chatCompletionService;
+	private final Kernel kernel;
+	private final InvocationContext invocationContext;
+
+	public ChatService(OpenAIAsyncClient openAIAsyncClient,
+										 ChatCompletionService chatCompletionService,
+										 PromptExecutionSettings promptExecutionSettings,
+										 Kernel kernel) {
+		this.openAIAsyncClient = openAIAsyncClient;
+		this.chatCompletionService = chatCompletionService;
+		this.kernel = kernel;
+
+		ContextVariableTypes
+				.addGlobalConverter(
+						ContextVariableTypeConverter.builder(Object.class)
+								.toPromptString(new Gson()::toJson)
+								.build());
+
+		this.invocationContext = new InvocationContext.Builder()
+				.withPromptExecutionSettings(promptExecutionSettings)
+				.withReturnMode(InvocationReturnMode.NEW_MESSAGES_ONLY)
+				.withToolCallBehavior(ToolCallBehavior.allowAllKernelFunctions(false))
+				.build();
+	}
+
+	public Mono<List<ChatMessageContent<?>>> getCompletion(String message) {
+		ChatHistory chatHistory = new ChatHistory()
+				.addUserMessage(message);
+		return chatCompletionService.getChatMessageContentsAsync(chatHistory, kernel, invocationContext);
+	}
+
+	public Flux<StreamingChatContent<?>> getCompletionStream(String message) {
+		ChatHistory chatHistory = new ChatHistory()
+				.addUserMessage(message);
+
+		return chatCompletionService.getStreamingChatMessageContentsAsync(chatHistory, kernel, invocationContext);
+	}
+
+	public Mono<List<ChatMessageContent<?>>> getCompletion(ChatHistory chatHistory) {
+		return chatCompletionService.getChatMessageContentsAsync(chatHistory, kernel, invocationContext);
+	}
+
+	public Flux<StreamingChatContent<?>> getCompletionStream(ChatHistory chatHistory) {
+
+		return chatCompletionService.getStreamingChatMessageContentsAsync(chatHistory, kernel, invocationContext);
+	}
+}
