f4c44a933bbc792482c6ca4208aff700ccb45364
gmezan <meza.gustavo@pucp.edu.pe>
2025-05-04 16:35:03 -0500

    app implementation

--- aks-openai-msi/src/test/java/com/example/openaimsi/AksOpenaiMsiApplicationTests.java
+++ aks-openai-msi/src/test/java/com/example/openaimsi/AksOpenaiMsiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.openaimsi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AksOpenaiMsiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
