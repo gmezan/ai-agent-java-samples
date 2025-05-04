c88c79d0ecc984cdd5a89654c0ea06868a8a5203
gmezan <meza.gustavo@pucp.edu.pe>
2025-05-04 15:38:05 -0500

    bootstraping aks openai msi

---
+++ aks-openai-msi/src/test/java/com/example/openaimsi/AksOpenaiMsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openaimsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AksOpenaiMsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
